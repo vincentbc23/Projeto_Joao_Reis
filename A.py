1777d2d879560a8d15ed9924fe0f18a0ed1ef3f4
joaoreiiis <jzreiis@proton.me>
2026-04-15 14:22:42 -0300

    criando classes A e B

---
+++ A.py
@@ -0,0 +1,22 @@
+class A:
+    def __init__(self, a1, a2):
+        self._a1 = a1
+        self._a2 = a2
+
+    def get_a1(self):
+        return self._a1
+
+    def set_a1(self, valor):
+        self._a1 = valor
+
+    def get_a2(self):
+        return self._a2
+
+    def set_a2(self, valor):
+        self._a2 = valor
+
+    def MA1(self):
+        print("Método MA1!")
+
+    def MA2(self):
+        print("Método MA2!")
---
+++ B.py
@@ -0,0 +1,17 @@
+class B:
+    def __init__ (self, b1, b2):
+        self._b1 = b1
+        self._b2 = b2
+
+    def get_b1(self):
+        return self._b1
+
+    def get_b2(self):
+        return self._b2
+
+    def set_b1(self, b):
+        self._b1 = b
+
+    def set_b2(self, b):
+        self._b2 = b
+
